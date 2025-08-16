90fdcaa903834947f5a514035be8564c24e7f6e6
Chris <christau90s@gmail.com>
2025-08-16 14:27:20 +0200

    feat: bitwarden login

---
+++ root/.chezmoiscripts/run_before_bitwarden-login.ps1
@@ -0,0 +1,19 @@
+bw login --check
+
+if (!$?) {
+    $session = bw login --raw
+    if ($session) {
+        # Set environment variable for current session
+        $env:BW_SESSION = $session
+        
+        # Persist environment variable for user
+        [System.Environment]::SetEnvironmentVariable("BW_SESSION", $session, "User")
+    }
+} else {
+    # If already logged in, ensure BW_SESSION is available
+    if (-not $env:BW_SESSION) {
+        $env:BW_SESSION = [System.Environment]::GetEnvironmentVariable("BW_SESSION", "User")
+    }
+}
+
+bw sync
